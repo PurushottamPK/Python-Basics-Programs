12baf5daa621fff75ce3f258fc754e24be502ee8
Purushottam Kumar <pkr.bca@gmail.com>
2021-05-17 12:19:34 +0530

    Create 3. Concatenate_String.py

---
+++ Unit-3 Expressions/3. Concatenate_String.py
@@ -0,0 +1,10 @@
+# <Prog_No:3> <Ex_No:2> <Author: Purushottam Kumar>
+# Write a program to concatenate the following string:
+'''Hi Everyone!!!”, “My name is ___________”, “I am a MCA I sem Student”,
+   My roll number is _________”. (First method: use all these strings inside your program;
+   Second Method: get the three strings as inputs from the user and then concatenate)'''
+
+print("\n Output of Prog_No:3 in Ex_No:2 implemented by Purushottam Kumar :\n")
+x=input(" Enter Your Name : ")
+y=input(" Enter Your Roll Number : ")
+print("\n Hi Everyone !!!, My name is : "+ x + ".\n I am a MCA I sem Student." + "\n My roll number is : "+ y) 
